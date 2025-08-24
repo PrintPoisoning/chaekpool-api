9ecd6d2bf1443d505e371d0a3bcea16757968610
ASak1104 <khw56184@gmail.com>
2025-08-24 22:55:52 +0900

    init: project files

---
+++ src/main/kotlin/io/chaekpool/ChaekpoolApplication.kt
@@ -0,0 +1,11 @@
+package io.chaekpool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChaekpoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChaekpoolApplication>(*args)
+}
---
+++ src/test/kotlin/io/chaekpool/ChaekpoolApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.chaekpool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChaekpoolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
