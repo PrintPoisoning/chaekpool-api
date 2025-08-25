00f78dda77f8f2a8e9e458d12849324046a27475
harry99.kim <harry99.kim@emro.co.kr>
2025-08-25 10:37:02 +0900

    style: reformat project

--- src/main/kotlin/io/chaekpool/ChaekpoolApplication.kt
+++ src/main/kotlin/io/chaekpool/ChaekpoolApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class ChaekpoolApplication
 
 fun main(args: Array<String>) {
-	runApplication<ChaekpoolApplication>(*args)
+    runApplication<ChaekpoolApplication>(*args)
 }
