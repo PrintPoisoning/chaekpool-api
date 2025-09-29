8b1a5491b68d9d99c83f49791781e42f1c331641
ASak1104 <khw56184@gmail.com>
2025-09-30 06:10:24 +0900

    feat: spring web security 설정 추가

---
+++ src/main/kotlin/io/chaekpool/common/config/AuthorizationRulesConfig.kt
@@ -0,0 +1,27 @@
+package io.chaekpool.common.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer
+
+typealias AuthRegistry = AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry
+
+@Configuration
+class AuthorizationRulesConfig {
+
+    @Bean
+    fun authorizationRules(): Customizer<AuthRegistry> {
+        return Customizer { auth ->
+            auth.requestMatchers("/api/v1/common/healthy")
+                .permitAll()
+                .requestMatchers("/api/v1/auth/**")
+                .permitAll()
+                .requestMatchers("/api/v1/oauth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+        }
+    }
+}
---
+++ src/main/kotlin/io/chaekpool/common/config/WebSecurityConfig.kt
@@ -0,0 +1,36 @@
+package io.chaekpool.common.config
+
+import io.chaekpool.auth.filter.JwtAuthenticationFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig(
+    private val jwtAuthenticationFilter: JwtAuthenticationFilter,
+    private val authorizationRules: Customizer<AuthRegistry>
+) {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return with(http) {
+            cors {}
+            csrf { it.disable() }
+            formLogin { it.disable() }
+            requestCache { it.disable() }
+            httpBasic { it.disable() }
+            rememberMe { it.disable() }
+            logout { it.disable() }
+            sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            authorizeHttpRequests(authorizationRules)
+            addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+            build()
+        }
+    }
+}
