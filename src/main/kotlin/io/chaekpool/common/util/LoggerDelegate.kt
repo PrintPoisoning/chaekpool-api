b096a88361e94a4cb20d566ec948132c2c718d98
ASak1104 <khw56184@gmail.com>
2025-09-30 04:23:46 +0900

    feat: 로깅 선언 유틸 추가
    private val log by LoggerDelegate()

---
+++ src/main/kotlin/io/chaekpool/common/util/LoggerDelegate.kt
@@ -0,0 +1,14 @@
+package io.chaekpool.common.util
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+class LoggerDelegate : ReadOnlyProperty<Any?, Logger> {
+
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): Logger {
+        return LoggerFactory.getLogger(thisRef?.javaClass)
+    }
+
+}
