09751d2fb2fbc3d144ef7aaf9c42517a018ad423
ASak1104 <khw56184@gmail.com>
2025-10-03 03:21:43 +0900

    feat: assertion 확장 함수 간소화

--- src/main/kotlin/io/chaekpool/common/util/AssertionExtension.kt
+++ src/main/kotlin/io/chaekpool/common/util/AssertionExtension.kt
@@ -1,9 +1,5 @@
 package io.chaekpool.common.util
 
-import io.chaekpool.common.exception.internal.BadRequestException
-import io.chaekpool.common.exception.internal.ForbiddenException
-import io.chaekpool.common.exception.internal.NotFoundException
-
 // Assert by Boolean
 inline fun Boolean.isTrueOrThrow(exception: () -> Throwable) {
     if (!this) {
@@ -11,35 +7,9 @@ inline fun Boolean.isTrueOrThrow(exception: () -> Throwable) {
     }
 }
 
-fun Boolean.isTrueOrBadRequest(message: String? = null) {
-    if (!this) {
-        throw message?.let { BadRequestException(it) } ?: BadRequestException()
-    }
-}
-
-fun Boolean.isTrueOrForbidden(message: String? = null) {
-    if (!this) {
-        throw message?.let { ForbiddenException(it) } ?: ForbiddenException()
-    }
-}
-
-fun Boolean.isTrueOrNotFound(message: String? = null) {
-    if (!this) {
-        throw message?.let { NotFoundException(it) } ?: NotFoundException()
-    }
-}
-
 // Assert by null
 inline fun <T> T?.notNullOrThrow(exception: () -> Throwable): T {
     return this ?: throw exception()
-}
-
-fun <T> T?.notNullOrBadRequest(message: String? = null): T {
-    return this ?: throw message?.let { BadRequestException(it) } ?: BadRequestException()
-}
-
-fun <T> T?.notNullOrNotFound(message: String? = null): T {
-    return this ?: throw message?.let { NotFoundException(it) } ?: NotFoundException()
 }
 
 
@@ -50,34 +20,10 @@ inline fun String?.hasTextOrThrow(exception: () -> Throwable): String {
     return this
 }
 
-fun String?.hasTextOrBadRequest(message: String? = null): String {
-    if (this.isNullOrBlank()) {
-        throw message?.let { BadRequestException(it) } ?: BadRequestException()
-    }
-
-    return this
-}
-
 // Assert by size
 inline fun <T, C : Collection<T>> C?.notEmptyOrThrow(exception: () -> Throwable): C {
     if (this.isNullOrEmpty()) {
         throw exception()
-    }
-
-    return this
-}
-
-fun <T, C : Collection<T>> C?.notEmptyOrNotFound(message: String? = null): C {
-    if (this.isNullOrEmpty()) {
-        throw message?.let { NotFoundException(it) } ?: NotFoundException()
-    }
-
-    return this
-}
-
-fun <T, C : Collection<T>> C?.hasSizeOrBadRequest(expected: Int, message: String? = null): C {
-    if (this == null || this.size != expected) {
-        throw message?.let { BadRequestException(it) } ?: BadRequestException()
     }
 
     return this
