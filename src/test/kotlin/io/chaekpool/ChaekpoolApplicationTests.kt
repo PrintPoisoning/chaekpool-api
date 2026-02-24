2cecc82d5799b60893cd4a8e6ceac8fca85c9af1
ASak1104 <khw56184@gmail.com>
2026-02-25 01:54:40 +0900

    test: ChaekpoolApplicationTests Kotest BehaviorSpec 전환
    - modify: ChaekpoolApplicationTests.kt (JUnit → Kotest BehaviorSpec, TestcontainersConfig Import)

--- src/test/kotlin/io/chaekpool/ChaekpoolApplicationTests.kt
+++ src/test/kotlin/io/chaekpool/ChaekpoolApplicationTests.kt
@@ -1,13 +1,16 @@
 package io.chaekpool
 
-import org.junit.jupiter.api.Test
+import io.chaekpool.support.TestcontainersConfig
+import io.kotest.core.spec.style.BehaviorSpec
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
 
 @SpringBootTest
-class ChaekpoolApplicationTests {
-
-    @Test
-    fun contextLoads() {
+@Import(TestcontainersConfig::class)
+class ChaekpoolApplicationTests : BehaviorSpec({
+    Given("Spring Boot 애플리케이션에서") {
+        When("컨텍스트를 로드하면") {
+            Then("정상적으로 기동된다") {}
+        }
     }
-
-}
+})
