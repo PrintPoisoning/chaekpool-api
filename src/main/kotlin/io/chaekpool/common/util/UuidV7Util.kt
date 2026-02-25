c2515534461103b75097af11da9a9f5b910e614a
ASak1104 <khw56184@gmail.com>
2026-02-26 07:21:38 +0900

    feat(common): UuidV7Util 유틸리티 추가
    - add: UuidV7Util.kt (UUID v7 생성 유틸리티)

---
+++ src/main/kotlin/io/chaekpool/common/util/UuidV7Util.kt
@@ -0,0 +1,27 @@
+package io.chaekpool.common.util
+
+import com.fasterxml.uuid.Generators
+import java.time.Instant
+import java.util.UUID
+
+object UuidV7Util {
+
+    private val generator = Generators.timeBasedEpochGenerator()
+
+    fun generate(): UUID = generator.generate()
+
+    fun extractTimestamp(uuid: UUID): Instant {
+        require(isUuidV7(uuid)) { "UUID version 7 required, but got version ${uuid.version()}" }
+
+        val epochMillis = uuid.mostSignificantBits ushr 16
+        return Instant.ofEpochMilli(epochMillis)
+    }
+
+    fun extractTimestamp(uuidString: String): Instant {
+        return extractTimestamp(UUID.fromString(uuidString))
+    }
+
+    fun isUuidV7(uuid: UUID): Boolean {
+        return uuid.version() == 7
+    }
+}
