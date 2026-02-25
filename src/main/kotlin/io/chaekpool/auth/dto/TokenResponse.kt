960014a694cf32a826d2efedbdd25e366d7d552d
ASak1104 <khw56184@gmail.com>
2026-02-26 07:22:25 +0900

    refactor(auth): TokenResponse 매직 스트링 상수화
    - modify: TokenResponse.kt (하드코딩된 JSON 프로퍼티명을 상수로 추출)

--- src/main/kotlin/io/chaekpool/auth/dto/TokenResponse.kt
+++ src/main/kotlin/io/chaekpool/auth/dto/TokenResponse.kt
@@ -2,9 +2,10 @@ package io.chaekpool.auth.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.chaekpool.auth.token.dto.TokenPair
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.ACCESS_TOKEN
 
 data class TokenResponse(
-    @param:JsonProperty("access_token")
+    @param:JsonProperty(ACCESS_TOKEN)
     val accessToken: String
 ) {
     constructor(tokenPair: TokenPair) : this(tokenPair.accessToken)
