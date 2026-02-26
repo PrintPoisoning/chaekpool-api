2dbb60431fb8ba15a28a5203949f547b30237cac
ASak1104 <khw56184@gmail.com>
2026-02-27 00:37:23 +0900

    test(common): UUIDv7Test 리네이밍 및 null 안전 테스트 추가
    - rename: UuidV7UtilTest.kt → UUIDv7Test.kt (클래스명 변경)
    - modify: UUIDv7Test.kt (null 파라미터 테스트 케이스 추가, shouldBeIn 매처 적용)

--- src/test/kotlin/io/chaekpool/common/util/UUIDv7Test.kt
+++ src/test/kotlin/io/chaekpool/common/util/UUIDv7Test.kt
@@ -2,29 +2,28 @@ package io.chaekpool.common.util
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
-import io.kotest.matchers.comparables.shouldBeGreaterThanOrEqualTo
 import io.kotest.matchers.comparables.shouldBeLessThan
-import io.kotest.matchers.comparables.shouldBeLessThanOrEqualTo
+import io.kotest.matchers.ranges.shouldBeIn
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import java.time.Instant
 import java.util.UUID
 
-class UuidV7UtilTest : BehaviorSpec({
+class UUIDv7Test : BehaviorSpec({
 
     Given("UUID v7을 생성할 때") {
         When("generate()를 호출하면") {
             Then("버전은 v7 이어야 한다") {
-                val uuid = UuidV7Util.generate()
+                val uuid = UUIDv7.generate()
 
                 uuid.version() shouldBe 7
-                UuidV7Util.isUuidV7(uuid) shouldBe true
+                UUIDv7.isUuidV7(uuid) shouldBe true
             }
 
             Then("시간순으로 정렬된다") {
-                val uuid1 = UuidV7Util.generate()
+                val uuid1 = UUIDv7.generate()
                 Thread.sleep(10)
-                val uuid2 = UuidV7Util.generate()
+                val uuid2 = UUIDv7.generate()
 
                 uuid1.toString() shouldBeLessThan uuid2.toString()
             }
@@ -35,22 +34,21 @@ class UuidV7UtilTest : BehaviorSpec({
         When("extractTimestamp(UUID)를 호출하면") {
             Then("발급 시간을 반환한다") {
                 val beforeGenerate = Instant.now().minusMillis(10)
-                val uuid = UuidV7Util.generate()
+                val uuid = UUIDv7.generate()
                 val afterGenerate = Instant.now().plusMillis(10)
 
-                val extracted = UuidV7Util.extractTimestamp(uuid)
+                val extracted = UUIDv7.extractTimestamp(uuid)
 
-                extracted shouldBeGreaterThanOrEqualTo beforeGenerate
-                extracted shouldBeLessThanOrEqualTo afterGenerate
+                extracted shouldBeIn beforeGenerate..afterGenerate
             }
         }
 
         When("extractTimestamp(String)을 호출하면") {
             Then("발급 시간을 반환한다") {
-                val uuid = UuidV7Util.generate()
+                val uuid = UUIDv7.generate()
                 val uuidString = uuid.toString()
 
-                val extracted = UuidV7Util.extractTimestamp(uuidString)
+                val extracted = UUIDv7.extractTimestamp(uuidString)
 
                 extracted shouldNotBe null
             }
@@ -61,8 +59,18 @@ class UuidV7UtilTest : BehaviorSpec({
                 val uuidV4 = UUID.randomUUID()
 
                 shouldThrow<IllegalArgumentException> {
-                    UuidV7Util.extractTimestamp(uuidV4)
+                    UUIDv7.extractTimestamp(uuidV4)
                 }
+            }
+        }
+
+        When("null을 전달하면") {
+            Then("IllegalArgumentException이 발생한다") {
+                val exception = shouldThrow<IllegalArgumentException> {
+                    UUIDv7.extractTimestamp(null as UUID?)
+                }
+
+                exception.message shouldBe "UUID version 7 required, but got version null"
             }
         }
     }
@@ -70,13 +78,17 @@ class UuidV7UtilTest : BehaviorSpec({
     Given("UUID 버전 확인 시") {
         When("isUuidV7()을 호출하면") {
             Then("UUID v7이면 true를 반환한다") {
-                val uuid = UuidV7Util.generate()
-                UuidV7Util.isUuidV7(uuid) shouldBe true
+                val uuid = UUIDv7.generate()
+                UUIDv7.isUuidV7(uuid) shouldBe true
             }
 
             Then("UUID v4이면 false를 반환한다") {
                 val uuidV4 = UUID.randomUUID()
-                UuidV7Util.isUuidV7(uuidV4) shouldBe false
+                UUIDv7.isUuidV7(uuidV4) shouldBe false
+            }
+
+            Then("null이면 false를 반환한다") {
+                UUIDv7.isUuidV7(null) shouldBe false
             }
         }
     }
