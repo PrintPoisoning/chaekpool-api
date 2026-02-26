5e2c40e055f152e98d9cf2343b385dcd576d28be
ASak1104 <khw56184@gmail.com>
2026-02-27 03:24:49 +0900

    fix(common): EncryptedStringSerializer Jackson 3.x 마이그레이션
    - modify: EncryptedStringSerializer.kt (com.fasterxml.jackson → tools.jackson ValueSerializer 전환)
    - modify: EncryptedStringDeserializer.kt (com.fasterxml.jackson → tools.jackson ValueDeserializer 전환)
    - modify: KakaoAuthTokenResponse.kt (@get:JsonDeserialize → @param:JsonDeserialize, tools.jackson 어노테이션 적용)
    
    Jackson 3.x의 JsonMapper가 2.x 커스텀 serializer를 인식하지 못하는 문제 수정
    JSONB 저장 시 accessToken/refreshToken 암호화가 정상 작동하도록 함

--- src/main/kotlin/io/chaekpool/auth/oauth2/dto/KakaoAuthTokenResponse.kt
+++ src/main/kotlin/io/chaekpool/auth/oauth2/dto/KakaoAuthTokenResponse.kt
@@ -1,8 +1,6 @@
 package io.chaekpool.auth.oauth2.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import io.chaekpool.common.serializer.EncryptedStringDeserializer
 import io.chaekpool.common.serializer.EncryptedStringSerializer
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.ACCESS_TOKEN
@@ -11,14 +9,16 @@ import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.RE
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.SCOPE
 import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.TOKEN_TYPE
 import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames.ID_TOKEN
+import tools.jackson.databind.annotation.JsonDeserialize
+import tools.jackson.databind.annotation.JsonSerialize
 
 data class KakaoAuthTokenResponse(
     @param:JsonProperty(TOKEN_TYPE)
     val tokenType: String,
 
     @param:JsonProperty(ACCESS_TOKEN)
+    @param:JsonDeserialize(using = EncryptedStringDeserializer::class)
     @get:JsonSerialize(using = EncryptedStringSerializer::class)
-    @get:JsonDeserialize(using = EncryptedStringDeserializer::class)
     val accessToken: String,
 
     @param:JsonProperty(ID_TOKEN)
@@ -28,8 +28,8 @@ data class KakaoAuthTokenResponse(
     val expiresIn: Long,
 
     @param:JsonProperty(REFRESH_TOKEN)
+    @param:JsonDeserialize(using = EncryptedStringDeserializer::class)
     @get:JsonSerialize(using = EncryptedStringSerializer::class)
-    @get:JsonDeserialize(using = EncryptedStringDeserializer::class)
     val refreshToken: String,
 
     @param:JsonProperty("refresh_token_expires_in")
--- src/main/kotlin/io/chaekpool/common/serializer/EncryptedStringSerializer.kt
+++ src/main/kotlin/io/chaekpool/common/serializer/EncryptedStringSerializer.kt
@@ -1,18 +1,21 @@
 package io.chaekpool.common.serializer
 
-import com.fasterxml.jackson.core.JsonGenerator
-import com.fasterxml.jackson.databind.JsonSerializer
-import com.fasterxml.jackson.databind.SerializerProvider
 import io.chaekpool.common.util.CryptoUtil
+import tools.jackson.core.JsonGenerator
+import tools.jackson.core.JsonParser
+import tools.jackson.databind.DeserializationContext
+import tools.jackson.databind.SerializationContext
+import tools.jackson.databind.ValueDeserializer
+import tools.jackson.databind.ValueSerializer
 
-class EncryptedStringSerializer : JsonSerializer<String>() {
-    override fun serialize(value: String, gen: JsonGenerator, serializers: SerializerProvider) {
+class EncryptedStringSerializer : ValueSerializer<String>() {
+    override fun serialize(value: String, gen: JsonGenerator, ctxt: SerializationContext) {
         gen.writeString(CryptoUtil.encrypt(value))
     }
 }
 
-class EncryptedStringDeserializer : com.fasterxml.jackson.databind.JsonDeserializer<String>() {
-    override fun deserialize(p: com.fasterxml.jackson.core.JsonParser, ctxt: com.fasterxml.jackson.databind.DeserializationContext): String {
-        return CryptoUtil.decrypt(p.text)
+class EncryptedStringDeserializer : ValueDeserializer<String>() {
+    override fun deserialize(p: JsonParser, ctxt: DeserializationContext): String {
+        return CryptoUtil.decrypt(p.getString())
     }
 }
